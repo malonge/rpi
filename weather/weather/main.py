bb3001bcde14a1c8a3ceceb22b0fa733fd66ad9f
malonge <malonge11@gmail.com>
2024-06-30 22:23:19 -0700

    cleaned up the the weather service code

--- weather/weather/main.py
+++ weather/weather/main.py
@@ -1,19 +1,44 @@
 #!/usr/bin/env python
 
 import os
+import json
+import time
+import logging
+from typing import Tuple
 
 import requests
 
-def get_nws_current_weather_data(latitude, longitude):
-    # Construct the URL for the NWS API endpoint
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(message)s',
+    level=logging.INFO
+)
+
+logger = logging.getLogger('weather service')
+
+
+def get_nws_current_weather_data(
+    latitude: float,
+    longitude: float
+) -> Tuple[float, float, str]:
+    """
+    Get current weather data from the national weather
+    service given coordinates.
+
+    Args:
+        latitude (float): Latitude for the location.
+        longitude (float): Longitude for the location.
+    Returns:
+        Tuple[float, float, str]: A tuple of temperature, humidity, and 
+            a description of the weather.
+    """
     points_url = f"https://api.weather.gov/points/{latitude},{longitude}"
     
-    # Get the station information
+    # Stations
     points_response = requests.get(points_url)
     points_data = points_response.json()
     observation_stations_url = points_data['properties']['observationStations']
     
-    # Get the list of observation stations
     stations_response = requests.get(observation_stations_url)
     stations_data = stations_response.json()
     observation_url = stations_data['features'][0]['id'] + "/observations/latest"
@@ -22,19 +47,36 @@ def get_nws_current_weather_data(latitude, longitude):
     observation_response = requests.get(observation_url)
     observation_data = observation_response.json()
     
-    # Extract relevant data
     temperature = observation_data['properties']['temperature']['value']
     humidity = observation_data['properties']['relativeHumidity']['value']
     description = observation_data['properties']['textDescription']
     
     return temperature, humidity, description
 
-def get_airnow_aqi(api_key, latitude, longitude):
-    url = "https://www.airnowapi.org/aq/observation/latLong/current"
-    headers = {
-        'Accept': 'application/json'  # Ensures the response is in JSON-LD format
-    }
 
+def get_airnow_aqi(
+    api_key: str,
+    latitude: float, 
+    longitude: float
+) -> Tuple[int, str]:
+    """
+    Fetches the current Air Quality Index (AQI) and category
+    from the AirNow API for the specified latitude and longitude.
+
+    Args:
+        api_key (str): API key for the AirNow API.
+        latitude (float): Latitude for the location.
+        longitude (float): Longitude for the location.
+
+    Returns:
+        Tuple[int, str]: AQI value and category name.
+
+    Raises:
+        requests.exceptions.HTTPError: If the API request fails.
+        ValueError: If the response data is empty or malformed.
+    """
+    url = "https://www.airnowapi.org/aq/observation/latLong/current"
+    headers = {'Accept': 'application/json'}
     params = {
         'format': 'application/json',
         'latitude': latitude,
@@ -42,29 +84,42 @@ def get_airnow_aqi(api_key, latitude, longitude):
         'distance': 25,
         'API_KEY': api_key
     }
-    print(params)
-    response = requests.get(url, headers=headers, params=params)
-    data = response.json()
-    print(data)
     
-    if response.status_code == 200 and data:
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    data = response.json()
+    
+    if data and len(data) > 1:
         aqi = data[1]['AQI']
         category = data[1]['Category']['Name']
         return aqi, category
-    else:
-        return None, None
 
-latitude = "34.0928"  # Hollywood, CA latitude
-longitude = "-118.3287"  # Hollywood, CA longitude
-airnow_api_key = os.environ.get("AIRNOW_KEY", "")
-if not airnow_api_key:
-    raise RuntimeError("Missing AirNow API Key in AIRNOW_KEY env variable")
+    raise ValueError("Invalid response data")
 
-temperature, humidity, description = get_nws_current_weather_data(latitude, longitude)
-aqi, category = get_airnow_aqi(airnow_api_key, latitude, longitude)
 
-print(f"Temperature: {temperature}°C")
-print(f"Humidity: {humidity}%")
-print(f"Description: {description}")
-print(f"AQI: {aqi} ({category})")
+def main():
+    latitude = "34.0928"
+    longitude = "-118.3287"
+    airnow_api_key = os.environ.get("AIRNOW_KEY", "")
+    if not airnow_api_key:
+        raise RuntimeError("Missing AirNow API Key in AIRNOW_KEY env variable")
+
+    while True:
+        temperature, humidity, description = get_nws_current_weather_data(latitude, longitude)
+        aqi, category = get_airnow_aqi(airnow_api_key, latitude, longitude)
+
+        data = {
+            "temperature": temperature,
+            "humidity": humidity,
+            "description": description,
+            "aqi": aqi,
+            "category": category
+        }
+
+        logger.info("Got the following weather data from the weather service: %s", json.dumps(data))
+        time.sleep(300)
+
+
+if __name__ == "__main__":
+    main()
 
